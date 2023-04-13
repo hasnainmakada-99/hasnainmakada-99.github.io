7dee96b0c4409f8cfc5150e298374439c86ffd8d
Hasnain Makada <hasnainmakada@gmail.com>
2023-04-14 02:13:19 +0530

    Testing hooks

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -15,7 +15,7 @@ const Skills = ({ content }) => {
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className=" w-full flex justify-center items-center flex-col mb-7">
           <p className="text-4xl font-bold inline border-b-4 border-cyan-500 text-center ">
-            Skills
+            Skills2
           </p>
           <p className="py-4 text-2xl">
             I enjoy diving into learning new things. Here's a list of
