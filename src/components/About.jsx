f5964086d100bc82b872b0b3050b92783acfdbfd
Hasnain Makada <hasnainmakada@gmail.com>
2023-04-13 16:48:37 +0530

    Added Responsiveness

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,22 +5,22 @@ const About = ({ content }) => {
     <div
       name="about"
       id="about"
-      className="w-full h-screen bg-[#0a192f] text-gray-300"
+      className="w-full min-h-screen bg-[#0a192f] text-gray-300 pt-20"
     >
-      <div className="flex flex-col justify-center items-center w-full h-full">
-        <div className=" py-16 rounded-md bg-cyan-800 flex flex-col justify-center items-center w-4/6">
-          <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8 mb-4">
-            <div className="sm:text-right pb-8 pl-4">
+      <div className="flex flex-col justify-center items-center w-full">
+        <div className="py-16 rounded-md bg-cyan-800 flex flex-col justify-center items-center w-4/5 md:w-4/6 lg:w-3/5 xl:w-1/2">
+          <div className="max-w-[1000px] w-full grid grid-cols-1 md:grid-cols-2 gap-8 mb-4">
+            <div className="text-center md:text-right pb-8 md:pb-0 md:pl-4">
               <p className="text-4xl font-bold inline border-b-4 border-cyan-500">
                 About
               </p>
             </div>
             <div></div>
           </div>
-          <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className="max-w-[1000px] w-full grid grid-cols-1 sm:grid-cols-2 gap-8 px-4">
             <div className="sm:text-auto text-2xl font-bold">
               <p>{content?.fields.about_caption}</p>
-              <p>Tech Geek | DevOps & Flutter 💙 | </p>
+              <p className="my-4">Tech Geek | DevOps & Flutter</p>
               <p>
                 Blogger at
                 <a href="https://hasnainm.hashnode.dev"> Hashnode</a> and
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,38 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
 
 import me2 from "../assets/Personal_Image.png";
-import { Link } from "react-scroll";
 
 const Home = ({ content }) => {
   return (
-    <div name="home" className="h-screen w-full bg-[#0a192f]">
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-        <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            {content?.fields.landing_caption}
-          </h2>
-          <br />
-          <div>
-            <Link
-              to="about"
-              smooth
-              duration={500}
-              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
-            >
-              About Me
-              <span className="group-hover:rotate-90 duration-300">
-                <HiArrowNarrowRight size={25} className="ml-3" />
-              </span>
-            </Link>
-          </div>
-        </div>
-        <div>
+    <div
+      name="home"
+      className="min-h-screen w-full bg-[#0a192f] flex justify-center items-center"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col-reverse md:flex-row items-center justify-center px-4">
+        <div className="flex-shrink-0 w-full md:w-auto mb-8 md:mb-0">
           <img
             src={me2}
             alt="my profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full"
+            className="rounded-2xl object-cover object-center h-60 sm:h-80 md:h-96 max-w-full sm:max-w-80 md:max-w-96"
           />
+        </div>
+        <div className="flex flex-col items-center justify-center text-center md:text-left md:ml-12">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white mb-8">
+            {content?.fields.landing_caption}
+          </h2>
+          <Link
+            to="about"
+            smooth
+            duration={500}
+            className="group text-white px-4 py-2 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer text-sm md:text-base"
+          >
+            About Me
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight size={20} className="ml-2" />
+            </span>
+          </Link>
         </div>
       </div>
     </div>
