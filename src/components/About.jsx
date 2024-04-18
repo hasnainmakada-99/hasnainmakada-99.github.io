da47bebf190d4390cc104722baf6f8c6a8036ddd
Hasnain Makada <hasnainmakada@gmail.com>
2024-04-18 11:32:56 +0530

    add: transition effects in home section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,42 +1,5 @@
 import React from "react";
-
-// const About = ({ content }) => {
-//   return (
-//     <div id="about" className="bg-[#0a192f] text-gray-300 min-h-screen py-20">
-//       <div className="container mx-auto">
-//         <div className="flex flex-col items-center">
-//           <div className="bg-cyan-800 rounded-md w-4/5 md:w-4/6 lg:w-3/5 xl:w-1/2 py-16">
-//             <div className="max-w-2xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 mb-4">
-//               <div className="text-center md:text-right">
-//                 <p className="text-4xl font-bold inline border-b-4 border-cyan-500">
-//                   About
-//                 </p>
-//               </div>
-//               <div></div>
-//             </div>
-//             <div className="max-w-2xl mx-auto grid grid-cols-1 sm:grid-cols-2 gap-8 px-4">
-//               <div className="text-2xl font-bold flex flex-col items-center">
-//                 <p>{content?.fields.about_caption}</p>
-//                 <p className="my-4">Tech Geek | DevOps & Flutter</p>
-//                 <p>
-//                   Blogger at
-//                   <a href="https://hasnainm.hashnode.dev"> Hashnode</a> and
-//                   <a href="https://showwcase.com/hasnainmakada-99">
-//                     {" "}
-//                     Showwcase
-//                   </a>
-//                 </p>
-//               </div>
-//               <div className="flex flex-col text-lg items-center">
-//                 <p>{content?.fields.about_details}</p>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+import { CSSTransition } from "react-transition-group";
 
 const About = ({ content }) => {
   return (
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,63 @@
-import React from "react";
+// import React from "react";
+// import { HiArrowNarrowRight } from "react-icons/hi";
+// import { Link } from "react-scroll";
+
+// import me2 from "../assets/New_Image.jpg";
+
+// const Home = ({ content }) => {
+//   return (
+//     <div
+//       name="home"
+//       className="min-h-screen w-full bg-[#0a192f] flex justify-center items-center"
+//     >
+//       <div className="max-w-screen-lg mx-auto flex flex-col-reverse md:flex-row items-center justify-center px-4">
+//         <div className="flex-shrink-0 w-full md:w-1/2 mb-8 md:mb-0">
+//           <img
+//             src={me2}
+//             alt="my profile"
+//             className="rounded-2xl flex-shrink-0 mx-auto object-cover object-center h-60 sm:h-80 md:h-96 max-w-full sm:max-w-80 md:max-w-96"
+//           />
+//         </div>
+
+//         <div className="flex flex-col items-center justify-center text-center md:text-left md:ml-12">
+//           <h2 className="text-4xl sm:text-7xl font-bold text-white mb-8">
+//             {content?.fields.landing_caption}
+//           </h2>
+//           <Link
+//             to="about"
+//             smooth
+//             duration={500}
+//             className="group text-white px-4 py-2 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer text-sm md:text-base"
+//           >
+//             About Me
+//             <span className="group-hover:rotate-90 duration-300">
+//               <HiArrowNarrowRight size={20} className="ml-2" />
+//             </span>
+//           </Link>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+import React, { useState, useEffect } from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import { Link } from "react-scroll";
 
 import me2 from "../assets/New_Image.jpg";
 
 const Home = ({ content }) => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoaded(true);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div
       name="home"
@@ -15,12 +68,22 @@ const Home = ({ content }) => {
           <img
             src={me2}
             alt="my profile"
+            style={{
+              transform: loaded ? "rotate(0deg)" : "rotate(-45deg)",
+              transition: "transform 1s",
+            }}
             className="rounded-2xl flex-shrink-0 mx-auto object-cover object-center h-60 sm:h-80 md:h-96 max-w-full sm:max-w-80 md:max-w-96"
           />
         </div>
 
         <div className="flex flex-col items-center justify-center text-center md:text-left md:ml-12">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white mb-8">
+          <h2
+            className="text-4xl sm:text-7xl font-bold text-white mb-8"
+            style={{
+              transform: loaded ? "rotate(0deg)" : "rotate(45deg)",
+              transition: "transform 1s",
+            }}
+          >
             {content?.fields.landing_caption}
           </h2>
           <Link
